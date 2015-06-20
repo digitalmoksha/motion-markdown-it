7f5c13add274404470fdfb9adfc0aae76b9c7aa7
Brett Walker <brett@digitalmoksha.com>
2015-06-20 18:52:21 +0200

    fix build

--- lib/motion-markdown-it.rb
+++ lib/motion-markdown-it.rb
@@ -10,6 +10,7 @@ if defined?(Motion::Project::Config)
   require 'linkify-it-rb'
   require 'mdurl-rb'
   require 'uc.micro-rb'
+  require 'bacon-expect' if ENV.fetch('TRAVIS') == 'true'  # make sure included in travis build
 
 else
   
