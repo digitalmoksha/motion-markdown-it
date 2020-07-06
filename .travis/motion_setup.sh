94e7df249ed911d3dcbee1b24407d9b39c13d4f9
digitalMoksha <brett@digitalmoksha.com>
2020-07-06 07:40:33 -0500

    Use motion_setup in travis

---
+++ .travis/motion_setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ $TRAVIS_OS_NAME = 'osx' ]; then
+  brew update
+  brew outdated xctool || brew upgrade xctool
+  (xcrun simctl list)
+  wget http://travisci.rubymotion.com/ -O RubyMotion-TravisCI.pkg
+  sudo installer -pkg RubyMotion-TravisCI.pkg -target /
+  cp -r /usr/lib/swift/*.dylib /Applications/Xcode.app/Contents/Frameworks/
+  touch /Applications/Xcode.app/Contents/Frameworks/.swift-5-staged
+  sudo mkdir -p ~/Library/RubyMotion/build
+  sudo chown -R travis ~/Library/RubyMotion
+  eval "sudo motion activate $RUBYMOTION_LICENSE"
+  sudo motion update
+  (motion --version)
+  (ruby --version)
+  motion repo
+else
+  # Install some custom requirements on Linux
+fi
